0defeb6bafc4859fdb52d41124c77c00e962081b
Nathan Kwon <kwon.nathan@gmail.com>
2016-09-01 01:20:12 +0800

    implemented the Street Class

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,58 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's street of address in the address book
+ * Guarantees: immutable; is valid as declared in {@link #isValidStreet(String)}
+ */
+public class Street {
+
+    public static final String EXAMPLE = "Clementi Ave 3";
+    public static final String MESSAGE_STREET_CONSTRAINTS = "Street can be in any format";
+    public static final String STREET_VALIDATION_REGEX = ".+";
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given street.
+     *
+     * @throws IllegalValueException if given block string is invalid.
+     */
+    public Street(String street, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        if (!isValidBlock(street)) {
+            throw new IllegalValueException(MESSAGE_STREET_CONSTRAINTS);
+        }
+        this.value = street;
+    }
+
+    /**
+     * Returns true if a given string is a valid person block.
+     */
+    public static boolean isValidBlock(String test) {
+        return test.matches(STREET_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Street // instanceof handles nulls
+                && this.value.equals(((Street) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
